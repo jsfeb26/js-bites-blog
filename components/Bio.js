c8fb8d5b8474ac673ece75efab28d5bbd9b68ed6
Jason Stinson <jsfeb26@gmail.com>
2017-07-02 17:05:39 -0400

    use styled-components add social links

--- components/Bio.js
+++ components/Bio.js
@@ -1,31 +1,67 @@
 import React from 'react';
 import { config } from 'config';
 import { rhythm } from 'utils/typography';
+import styled from 'styled-components';
+
 import profilePic from '../images/profile-picture.png';
+import linkedin from '../images/linkedin.png';
+import twitter from '../images/twitter.png';
+import github from '../images/github.png';
 
-class Bio extends React.Component {
-  render () {
-    return (
-      <p
-        style={{
-          marginBottom: rhythm(2.5),
-        }}
-      >
-        <img
-          src={profilePic}
-          alt={`author ${config.authorName}`}
-          style={{
-            float: 'left',
-            marginRight: rhythm(1/4),
-            marginBottom: 0,
-            width: rhythm(2),
-            height: rhythm(2),
-          }}
-        />
-        Written by <strong>{config.authorName}</strong> who lives and works in New York building useful things. <a href="https://twitter.com/jsfeb26">You should follow him on Twitter</a>
-      </p>
-    )
+const BioContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 120px;
+`;
+
+const SocialLinks = styled.a`
+  display: block;
+  box-shadow: none;
+  width: 32px;
+  height: 32px;
+`;
+
+const SocialImages = styled.img`
+  width: 32px;
+  height: 32px;
+  margin-bottom: 0;
+  &:hover {
+    opacity: 0.5;
   }
-}
+`;
 
-export default Bio;
+const BioParagraph = styled.p`
+  text-align: center;
+  margin-bottom: 36px;
+`;
+
+const ProfilePic = styled.img`
+  width: 100px;
+  margin-bottom: 9px;
+`;
+
+export default () => (
+  <BioContainer>
+    <ProfilePic src={profilePic} alt={`author ${config.authorName}`} />
+    <BioParagraph>
+      Written by <strong>{config.authorName}</strong> who lives and works in New York building useful things. <a href="https://twitter.com/jsfeb26">You should follow him on Twitter</a>
+    </BioParagraph>
+    <SocialContainer>
+      <SocialLinks href="https://www.linkedin.com/in/jsfeb26/">
+        <SocialImages alt="Linkedin" src={linkedin} />
+      </SocialLinks>
+      <SocialLinks href="https://twitter.com/jsfeb26">
+        <SocialImages alt="Twitter" src={twitter} />
+      </SocialLinks>
+      <SocialLinks href="github.com/jsfeb26">
+        <SocialImages alt="Github" src={github} />
+      </SocialLinks>
+    </SocialContainer>
+  </BioContainer>
+);
---
+++ gatsby-config.js
@@ -0,0 +1,3 @@
+plugins: [
+  `gatsby-plugin-styled-components`
+]
